ca228f222fc30c1e5de2ff940d1427df07a43a89
Mokshagna Sai Teja Komatipolu <mokshagnasaiteja517@gmail.com>
2020-04-02 16:00:06 +0530

    Modal added

--- js/hangman.js
+++ js/hangman.js
@@ -49,9 +49,22 @@ function handleGuess(chosenLetter) {
     checkIfGameWon();
   } else if (answer.indexOf(chosenLetter) === -1) {
     mistakes++;
+
     updateMistakes();
     checkIfGameLost();
     updateHangmanPicture();
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+    span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
   }
 }
 
